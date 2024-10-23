75da348bc36c48b83f53b484ae0d1bb7766953a2
cle-b <cle@tictac.pm>
2024-10-23 21:59:55 +0200

    fix windows

--- tests/test_hosts_only.py
+++ tests/test_hosts_only.py
@@ -12,7 +12,6 @@ from hookdns import hosts
 def test_patch_decorator_only_gethostbyname():
     assert socket.gethostbyname("localhost") == "1.2.3.4"
     (hostname, _, ipaddrlist) = socket.gethostbyname_ex("localhost")
-    assert hostname == "localhost"
     assert "127.0.0.1" in ipaddrlist
     r = socket.getaddrinfo(
         "localhost", 80, family=socket.AF_INET, proto=socket.IPPROTO_TCP
@@ -24,7 +23,7 @@ def test_patch_decorator_only_gethostbyname():
 @hosts({"localhost": "1.2.3.4"}, only=["gethostbyname_ex"])
 def test_patch_decorator_only_gethostbyname_ex():
     assert socket.gethostbyname("localhost") == "127.0.0.1"
-    assert socket.gethostbyname_ex("localhost") == ("localhost", [], ["1.2.3.4"])
+    assert socket.gethostbyname_ex("localhost")[2] == ["1.2.3.4"]
     r = socket.getaddrinfo(
         "localhost", 80, family=socket.AF_INET, proto=socket.IPPROTO_TCP
     )
@@ -36,7 +35,6 @@ def test_patch_decorator_only_gethostbyname_ex():
 def test_patch_decorator_only_getaddrinfo():
     assert socket.gethostbyname("localhost") == "127.0.0.1"
     (hostname, _, ipaddrlist) = socket.gethostbyname_ex("localhost")
-    assert hostname == "localhost"
     assert "127.0.0.1" in ipaddrlist
     r = socket.getaddrinfo(
         "localhost", 80, family=socket.AF_INET, proto=socket.IPPROTO_TCP
@@ -48,7 +46,7 @@ def test_patch_decorator_only_getaddrinfo():
 @hosts({"localhost": "1.2.3.4"}, only=["gethostbyname", "gethostbyname_ex"])
 def test_patch_decorator_only_gethostbyname_and_gethostbyname_ex():
     assert socket.gethostbyname("localhost") == "1.2.3.4"
-    assert socket.gethostbyname_ex("localhost") == ("localhost", [], ["1.2.3.4"])
+    assert socket.gethostbyname_ex("localhost")[2] == ["1.2.3.4"]
     r = socket.getaddrinfo(
         "localhost", 80, family=socket.AF_INET, proto=socket.IPPROTO_TCP
     )
@@ -63,7 +61,6 @@ def test_patch_contextmanager_only_gethostbyname():
     with hosts({"localhost": "1.2.3.4"}, only=["gethostbyname"]):
         assert socket.gethostbyname("localhost") == "1.2.3.4"
         (hostname, _, ipaddrlist) = socket.gethostbyname_ex("localhost")
-        assert hostname == "localhost"
         assert "127.0.0.1" in ipaddrlist
         r = socket.getaddrinfo(
             "localhost", 80, family=socket.AF_INET, proto=socket.IPPROTO_TCP
@@ -75,7 +72,7 @@ def test_patch_contextmanager_only_gethostbyname():
 def test_patch_contextmanager_only_gethostbyname_ex():
     with hosts({"localhost": "1.2.3.4"}, only=["gethostbyname_ex"]):
         assert socket.gethostbyname("localhost") == "127.0.0.1"
-        assert socket.gethostbyname_ex("localhost") == ("localhost", [], ["1.2.3.4"])
+        assert socket.gethostbyname_ex("localhost")[2] == ["1.2.3.4"]
         r = socket.getaddrinfo(
             "localhost", 80, family=socket.AF_INET, proto=socket.IPPROTO_TCP
         )
@@ -87,7 +84,6 @@ def test_patch_contextmanager_only_getaddrinfo():
     with hosts({"localhost": "1.2.3.4"}, only=["getaddrinfo"]):
         assert socket.gethostbyname("localhost") == "127.0.0.1"
         (hostname, _, ipaddrlist) = socket.gethostbyname_ex("localhost")
-        assert hostname == "localhost"
         assert "127.0.0.1" in ipaddrlist
         r = socket.getaddrinfo(
             "localhost", 80, family=socket.AF_INET, proto=socket.IPPROTO_TCP
@@ -99,7 +95,7 @@ def test_patch_contextmanager_only_getaddrinfo():
 def test_patch_contextmanager_only_gethostbyname_and_gethostbyname_ex():
     with hosts({"localhost": "1.2.3.4"}, only=["gethostbyname", "gethostbyname_ex"]):
         assert socket.gethostbyname("localhost") == "1.2.3.4"
-        assert socket.gethostbyname_ex("localhost") == ("localhost", [], ["1.2.3.4"])
+        assert socket.gethostbyname_ex("localhost")[2] == ["1.2.3.4"]
         r = socket.getaddrinfo(
             "localhost", 80, family=socket.AF_INET, proto=socket.IPPROTO_TCP
         )
